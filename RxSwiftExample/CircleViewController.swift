c2447ce9c8cb69da59d2e151e316c6ed586303cb
dpffpsk <gpljw0419@gmail.com>
2023-06-01 11:05:09 +0900

    CircleVC 코드 구현 완료

--- RxSwiftExample/CircleViewController.swift
+++ RxSwiftExample/CircleViewController.swift
@@ -27,12 +27,13 @@ class CircleViewController: UIViewController {
         setup()
         setupBinding()
         
+        // 제스처 추가
         let gestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(circleMoved(_:)))
         circleView.addGestureRecognizer(gestureRecognizer)
     }
 
     func setup() {
-        // 원 모양의 뷰
+        // 원 모양의 뷰를 그린다.
         circleView = UIView(frame: CGRect(origin: view.center, size: CGSize(width: 100.0, height: 100.0)))
         circleView.layer.cornerRadius = circleView.frame.width / 2.0
         circleView.center = view.center
@@ -41,6 +42,7 @@ class CircleViewController: UIViewController {
     }
     
     @objc func circleMoved(_ recognizer: UIPanGestureRecognizer) {
+        // 이동 위치를 가져와 원의 중심 좌표로 지정해준다.
         let location = recognizer.location(in: view)
         UIView.animate(withDuration: 0.1) {
             self.circleView.center = location
@@ -48,23 +50,32 @@ class CircleViewController: UIViewController {
     }
     
     func setupBinding() {
+        // Observer(송신)
+        // 원의 중앙 위치 값을 관찰한다.(원에서 새로운 위치 값이 반환되는 것을 관찰.)
+        // bind : 값을 넘겨줄 때 사용
         circleView.rx.observe(CGPoint.self, "center")
-            .filter{ $0 != nil }.map{ $0! }
+//            .filter{ $0 != nil }
+            .map{ $0! } // 이 코드가 없이 bind를 사용할 시 오류가 난다. optional 체크를 하지 않았기때문이다.
             .bind(to: circleViewModel.centerObservable)
             .disposed(by: disposeBag)
         
         
+        // Observer -> Observable 발생된 이벤트 다룬다.
+        // subscribe : 한 대상이 있고 subscribe를 하고 있을 때, 그 대상의 상태가 변하면 그 값을 받아 와서 처리할 수 있게한다.
         circleViewModel.backgroundColorObservable.subscribe { [weak self] backgroundColor in
             UIView.animate(withDuration: 0.1) {
+                // 원 색 바꾸기
                 self?.circleView.backgroundColor = backgroundColor
                 
                 let r = backgroundColor.components.red
                 let g = backgroundColor.components.green
                 let b = backgroundColor.components.blue
                 
+                // truncatingRemainder(dividingBy:) : 소수점이 있는 Double, Float의 나머지 값
                 let viewBackgroundColor: UIColor = UIColor.init(red: 1-r.truncatingRemainder(dividingBy: 0.4), green: 1-g.truncatingRemainder(dividingBy: 0.4), blue: 1-b.truncatingRemainder(dividingBy: 0.4), alpha: 1.0)
                 
                 if backgroundColor != viewBackgroundColor {
+                    // 배경화면 색 바꾸기
                     self?.view.backgroundColor = viewBackgroundColor
                 }
             }
--- RxSwiftExample/CircleViewModel.swift
+++ RxSwiftExample/CircleViewModel.swift
@@ -13,7 +13,8 @@ import UIKit
 class CircleViewModel {
     
     var centerObservable: BehaviorSubject<CGPoint> = BehaviorSubject(value: CGPoint(x: 0.0, y: 0.0)) // subscribe한 원의 가장 최근 중앙 지점만 가져옴
-    var backgroundColorObservable: Observable<UIColor>!
+    var backgroundColorObservable: BehaviorSubject<UIColor> = BehaviorSubject(value: UIColor.black)
+//    var backgroundColorObservable: Observable<UIColor>!
     var disposeBag = DisposeBag()
     
     init() {
@@ -21,22 +22,35 @@ class CircleViewModel {
     }
     
     func setup() {
+        // Observable(수신)
         // 새로운 중앙 값을 받으면, 새로운 UIColor를 반환
-        backgroundColorObservable = centerObservable.asObservable().map({ center in
-            
-//            guard let center = center else { return UIColor.black } // Optional이기 때문에 디폴트 색상을 black으로 지정
-            
-            // 새로운 CGPoint 값을 UIColor로 연결
+        // bind : 값을 넘겨줄 때 사용
+        centerObservable.map { center in
             var r: CGFloat = ((center.x + center.y).truncatingRemainder(dividingBy: 255) / 255.0)
-            let g: CGFloat = 0.0
-            let b: CGFloat = 0.0
+            let g: CGFloat = 0.1
+            let b: CGFloat = 0.1
             
-//            let rs = String(format: "%.1f", r)
-//            r = self.stringToCgFloat(rs)
+            let rs = String(format: "%.1f", r) // 소수점 아래 1자리까지
+            r = self.stringToCgFloat(rs)
             
             return UIColor.init(red: r, green: g, blue: b, alpha: 1.0)
-            
-        })
+        }
+        .bind(to: backgroundColorObservable)
+        .disposed(by: disposeBag)
+        
+        // Subject.asObservable() : Subject는 Observer와 Observable 역할을 다 하는데, 외부에서 Observer에 접근하지 못하도록 설정하며 Observable에만 접근할 수 있도록 나눠서 접근 가능하도록 하기위함
+//        backgroundColorObservable = centerObservable.asObservable().map({ center in
+//
+//            // 새로운 CGPoint 값을 UIColor로 연결
+//            var r: CGFloat = ((center.x + center.y).truncatingRemainder(dividingBy: 255) / 255.0)
+//            let g: CGFloat = 0.0
+//            let b: CGFloat = 0.0
+//
+//            let rs = String(format: "%.1f", r) // 소수점 아래 1자리까지
+//            r = self.stringToCgFloat(rs)
+//
+//            return UIColor.init(red: r, green: g, blue: b, alpha: 1.0)
+//        })
     }
     
     func stringToCgFloat(_ s: String) -> CGFloat {
