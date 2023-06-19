c8d57cd4649482db6689a8fe2aa360b978b8de1a
dpffpsk <gpljw0419@gmail.com>
2023-06-19 10:34:43 +0900

    TextField 유효성 로직 추가, 로그인 버튼 이벤트 추가

--- RxSwiftExample/Login/LoginViewController.swift
+++ RxSwiftExample/Login/LoginViewController.swift
@@ -14,6 +14,13 @@ class LoginViewController: UIViewController {
     let loginView = LoginView()
     let disposeBag = DisposeBag()
     
+    // BehaviorSubject : 최근 값을 저장해준다, 초기값 설정 가능
+    let idText: BehaviorSubject<String> = BehaviorSubject(value: "")
+    let passwordText: BehaviorSubject<String> = BehaviorSubject(value: "")
+    
+    let idValid: BehaviorSubject<Bool> = BehaviorSubject(value: false)
+    let passwordValid: BehaviorSubject<Bool> = BehaviorSubject(value: false)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -33,13 +40,68 @@ class LoginViewController: UIViewController {
     }
     
     // orEmpty : 옵셔널 String 걸러줌
-    // skip : 지정한 횟수 만큼 이벤트를 skip 해줌
-    // distinctUntilChanged : 이전 값과 비교하여 값이 같으면 건너뜀
     func setupBinding() {
+        // input
+        loginView.idTextField.rx.text.orEmpty
+            .bind(to: idText)
+            .disposed(by: disposeBag)
+        
+        idText.map(checkId(_:))
+            .bind(to: idValid)
+            .disposed(by: disposeBag)
+        
+        loginView.passwordTextField.rx.text.orEmpty
+            .bind(to: passwordText)
+            .disposed(by: disposeBag)
+        
+        passwordText.map(checkPassword(_:))
+            .bind(to: passwordValid)
+            .disposed(by: disposeBag)
+        
+        
+        // output
+        idValid.subscribe(onNext: {
+            if $0 {
+                self.loginView.colorBoxView1.backgroundColor = .systemBlue
+            } else {
+                self.loginView.colorBoxView1.backgroundColor = .systemRed
+            }
+        })
+        .disposed(by: disposeBag)
+        
+        passwordValid.subscribe(onNext: {
+            if $0 {
+                self.loginView.colorBoxView2.backgroundColor = .systemBlue
+            } else {
+                self.loginView.colorBoxView2.backgroundColor = .systemRed
+            }
+        })
+        .disposed(by: disposeBag)
+        
+        Observable.combineLatest(
+            idValid,
+            passwordValid,
+            resultSelector: { $0 && $1 }
+        )
+        .subscribe(onNext: {
+            self.loginView.loginButton.isEnabled = $0
+        })
+        .disposed(by: disposeBag)
+        
+        
+        // 로그인 버튼 탭
+        loginView.loginButton.rx.tap
+            .bind {
+                self.view.endEditing(true)
+                print("Button Clicked")
+            }
+            .disposed(by: disposeBag)
+        
+        /*
+        // 이메일 입력
         loginView.idTextField.rx.text
             .orEmpty
-            .map({ return $0.contains("@") && $0.contains(".") })
-            .distinctUntilChanged()
+            .map(checkId(_:))
             .subscribe(onNext: {
                 if $0 {
                     self.loginView.colorBoxView1.backgroundColor = .systemBlue
@@ -49,11 +111,11 @@ class LoginViewController: UIViewController {
             })
             .disposed(by: disposeBag)
         
+        // 비밀번호 입력
         loginView.passwordTextField.rx.text
             .orEmpty
             .map(checkPassword(_:))
-            .distinctUntilChanged()
-            .observe(on: MainScheduler.asyncInstance)
+//            .observe(on: MainScheduler.asyncInstance)
             .subscribe(onNext: {
                 if $0 {
                     self.loginView.colorBoxView2.backgroundColor = .systemBlue
@@ -62,6 +124,24 @@ class LoginViewController: UIViewController {
                 }
             })
             .disposed(by: disposeBag)
+        
+        // 로그인 버튼 활성화
+        // combineLatest : 하나라도 바뀌면 실행
+        Observable.combineLatest(
+            loginView.idTextField.rx.text.orEmpty.map(checkId(_:)),
+            loginView.passwordTextField.rx.text.orEmpty.map(checkPassword(_:)),
+            resultSelector: {s1, s2 in s1 && s2}
+        )
+        .subscribe(onNext: { s in // s1 && s2 결과 값
+            self.loginView.loginButton.isEnabled = s
+            
+        })
+        .disposed(by: disposeBag)
+         */
+    }
+    
+    private func checkId(_ str: String) -> Bool {
+        return str.contains("@") && str.contains(".")
     }
     
     private func checkPassword(_ str: String) -> Bool {
