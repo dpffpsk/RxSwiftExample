a3ac158c02f85dbfc32d04a0b35698e688ebb813
dpffpsk <gpljw0419@gmail.com>
2023-07-20 17:22:58 +0900

    RxMemo 수정

--- RxSwiftExample/RxMemo/ViewController/MemoListViewController.swift
+++ RxSwiftExample/RxMemo/ViewController/MemoListViewController.swift
@@ -11,13 +11,29 @@ class MemoListViewController: UIViewController, ViewModelBindableType {
 
     var viewModel: MemoListViewModel!
     
+    let tableview = UITableView()
+    let addButton = UIBarButtonItem()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        title = "메모 목록"
+
     }
     
     func bindViewModel() {
+        
+    }
+    
+    func attribute() {
+        title = "메모 목록"
+        view.backgroundColor = .white
+        
+        addButton.style = .plain
+        addButton.title = "ADD"
+        
+        ad
+    }
+    
+    func layout() {
         
     }
 }
---
+++ RxSwiftExample/RxMemo/ViewModel/CommonViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  CommonViewModel.swift
+//  RxSwiftExample
+//
+//  Created by jiweon.lee on 2023/07/18.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class CommonViewModel: NSObject {
+    let title: Driver<String>
+    let sceneCoordinator: SceneCoordinatorType
+    let storage: MemoStorageType
+    
+    init(title: String, sceneCoordinator: SceneCoordinatorType, storage: MemoStorageType) {
+        self.title = Observable.just(title).asDriver(onErrorJustReturn: "")
+        self.sceneCoordinator = sceneCoordinator
+        self.storage = storage
+    }
+}
