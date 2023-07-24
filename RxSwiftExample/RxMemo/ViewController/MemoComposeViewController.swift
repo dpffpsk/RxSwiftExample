2d93b246debef9b5eb36d964774b7bbf104afad3
dpffpsk <gpljw0419@gmail.com>
2023-07-24 16:49:34 +0900

    RxMemo Compose 기능 개발 및 수정 중

--- RxSwiftExample/RxMemo/ViewController/MemoComposeViewController.swift
+++ RxSwiftExample/RxMemo/ViewController/MemoComposeViewController.swift
@@ -6,17 +6,82 @@
 //
 
 import UIKit
+import RxSwift
+import SnapKit
 
 class MemoComposeViewController: UIViewController, ViewModelBindableType {
 
+    let disposeBag = DisposeBag()
     var viewModel: MemoComposeViewModel!
+    
+    let cancelButton = UIBarButtonItem()
+    let saveButton = UIBarButtonItem()
+    let contentTextView = UITextView()
+    
+    let test = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        attribute()
+        layout()
+        
+        test.addTarget(self, action: #selector(<#T##@objc method#>), for: <#T##UIControl.Event#>)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        contentTextView.becomeFirstResponder()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if contentTextView.isFirstResponder {
+            contentTextView.resignFirstResponder()
+        }
     }
 
     func bindViewModel() {
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: disposeBag)
+        
+        // 쓰기모드에서는 빈 문자열, 편집모드에서는 편집할 문자열 표시
+        viewModel.initialText
+            .drive(contentTextView.rx.text)
+            .disposed(by: disposeBag)
+        
+        // 취소액션 바인딩
+        cancelButton.rx.action = viewModel.cancelAction
+        // throttle : 더블탭방지
+        // withLatestFrom 텍스트 뷰에 입력된 텍스트 방출
+        saveButton.rx.tap
+            .throttle(.milliseconds(500), scheduler: MainScheduler.instance)
+            .withLatestFrom(contentTextView.rx.text.orEmpty)
+            .bind(to: viewModel.saveAction.inputs)
+            .disposed(by: disposeBag)
+    }
+    
+    func attribute() {
+        title = "새 메모"
+        view.backgroundColor = .white
+        
+        // barButton
+        cancelButton.title = "Cancel"
+        navigationItem.setLeftBarButton(cancelButton, animated: true)
+        
+        saveButton.title = "Save"
+        navigationItem.setRightBarButton(saveButton, animated: true)
 
+        
+    }
+    
+    func layout() {
+        view.addSubview(contentTextView)
+        
+        contentTextView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 }
--- RxSwiftExample/RxMemo/ViewModel/MemoComposeViewModel.swift
+++ RxSwiftExample/RxMemo/ViewModel/MemoComposeViewModel.swift
@@ -6,7 +6,44 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
-class MemoComposeViewModel {
+class MemoComposeViewModel: CommonViewModel {
+    private let content: String?
     
+    var initialText: Driver<String?> {
+        return Observable.just(content).asDriver(onErrorJustReturn: nil)
+    }
+    
+    let saveAction: Action<String, Void>
+    let cancelAction: CocoaAction
+    
+    init(title: String, content: String? = nil, sceneCoordinator: SceneCoordinatorType storage: MemoStorageType, saveAction: Action<String, Void>? = nil, cancelAction: CocoaAction? = nil ) {
+        self.content = content
+        
+        // 뷰모델에서 버튼 액션을 구현하면 처리 방식이 하나로 고정됨
+        // 뷰모델에서 파라미터로 버튼 액션을 받으면 이전화면에서 처리 방식을 동적으로 결정할 수 있음
+        
+        // 버튼 저장 액션
+        // 버튼 액션이 전달되면 액션 실행 후 화면을 닫음
+        self.saveAction = Action<String, Void> { input in
+            if let action = saveAction {
+                action.execute(input)
+            }
+            
+            return SceneCoordinator.close(animated: )
+        }
+        
+        // 버튼 취소 액션
+        self.cancelAction = CocoaAction {
+            if let action = cancelAction {
+                action.execute()
+            }
+            
+            return sceneCoordinator.close(animated: true).asObservable().map{ _ in }
+        }
+        
+        super.init(title: title, storage: storage)
+    }
 }
